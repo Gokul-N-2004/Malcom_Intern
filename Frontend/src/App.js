f55b9f4f0dcf30252ec75a61ba1d11426dd82330
jaya01092005 <jayamanik2005@gmail.com>
2024-11-17 11:58:25 +0530

    code update

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutPage from "./AboutPage"; //import AboutPage from "./aboutpage";
 
 function App() {
   return (
     <div className="App">
-      
       <h1>Gokul N</h1>
     </div>
   );
 }
 
-export default App;
+export default AboutPage;
